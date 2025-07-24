8fa745c39e581f1e66e790b76c55b6b034115e44
liandracruz <liandraluiza.02@gmail.com>
2025-07-24 16:55:17 -0300

    learning about data types and variables

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,7 @@
-let country = 'Spain';
-let continent = 'Europe';
+const country = 'Spain';
+const continent = 'Europe';
 let population = '48.35 million (2023)';
+const isIsland = false;
+const language = 'Spanish';
 
-console.log(country, continent, population);
+console.log(country, continent, population, isIsland, language);
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,32 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 let firstName = 'Jonas';
 console.log(firstName);
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Lia');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(year)
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1994;
