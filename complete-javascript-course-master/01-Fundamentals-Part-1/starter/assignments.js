c280c5206ab735dfa22dae9877f683818a574561
liandracruz <liandraluiza.02@gmail.com>
2025-07-28 17:21:20 -0300

    learning operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,14 @@
 const country = 'Spain';
 const continent = 'Europe';
-let population = '48.35 million (2023)';
+let population = 48.35;
 const isIsland = false;
 const language = 'Spanish';
 
-console.log(country, continent, population, isIsland, language);
+let halfPopulation = 48.35 / 2;
+const finlandPopulation = 6;
+const averagePopulation = 33;
+
+let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(country, continent, population, isIsland, language, halfPopulation, finlandPopulation > country, averagePopulation > country);
+console.log(description)
