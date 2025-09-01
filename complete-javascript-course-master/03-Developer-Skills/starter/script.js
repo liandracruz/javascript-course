2fda54101eff05c7a21ef55b4e10a9ea65a21d6c
liandracruz <liandraluiza.02@gmail.com>
2025-09-01 15:19:44 -0300

    practicing

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,6 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
-const calcAge = birthYear => 2037 - birthYear;
+//Practicing
+const temperatures1 = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [7, 12, 18, 'error', 22, 26, 30];
+const temps = temperatures1.concat(temperatures2);
+console.log(temps);
 
-console.log();
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temps);
+console.log(amplitude);
