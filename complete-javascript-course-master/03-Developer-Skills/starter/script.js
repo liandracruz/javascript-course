ce8b2e7bf856be11b30fb2e95809360972aa8f3a
liandracruz <liandraluiza.02@gmail.com>
2025-09-05 10:27:25 -0300

    practicing

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
+/*
 //Practicing
 const temperatures1 = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 const temperatures2 = [7, 12, 18, 'error', 22, 26, 30];
@@ -24,3 +25,22 @@ const calcTempAmplitude = function (temps) {
 
 const amplitude = calcTempAmplitude(temps);
 console.log(amplitude);
+*/
+
+const measureKelvin = function() {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degrees celsius:')),
+  };
+
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
