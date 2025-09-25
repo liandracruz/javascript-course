bbfcf24c73d46ff616ad7ea0fa9ea1bdd7191981
liandracruz <liandraluiza.02@gmail.com>
2025-09-25 11:28:06 -0300

    secret number game - finished

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -10,9 +10,9 @@ document.querySelector('.guess').value = 26;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +26,13 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        document.querySelector('.number').textContent = secretNumber
+
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore
+        } 
 
     } else if(guess > secretNumber) {
         if(score > 1) {
@@ -48,4 +55,17 @@ document.querySelector('.check').addEventListener('click', function() {
 
 
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 })
