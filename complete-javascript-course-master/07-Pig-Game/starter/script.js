6d38f278d9e8c24f4d1860ae7f8e31552aed0233
liandracruz <liandraluiza.02@gmail.com>
2025-10-07 10:12:28 -0300

    finishing JavaScript

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -11,14 +11,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -45,18 +57,24 @@ btnRoll.addEventListener('click', function() {
     }
 });
 
-btnHold.addEventListener('click', function() {
-    if(playing) {
-        scores[activePlayer] += currentScore;
-        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-
-        if(scores[activePlayer] >= 20) {
-            playing = false;
-            diceEl.classList.add('hidden');
-            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-        } else {
-            switchPlayer();
-        }
+btnHold.addEventListener('click', function () {
+    if (playing) {
+      scores[activePlayer] += currentScore;
+  
+      document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+  
+      if (scores[activePlayer] >= 20) {
+        playing = false;
+        diceEl.classList.add('hidden');
+  
+        // document.querySelector(`.player--${activePlayer}`);
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+      } else {
+        switchPlayer();
+      }
     }
-});
+  });
+
+
+btnNew.addEventListener('click', init);
