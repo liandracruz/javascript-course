2f866334ab6bf70b4e184cf8b0b05ee11501c914
liandracruz <liandraluiza.02@gmail.com>
2025-10-20 09:41:29 -0300

    practicing

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -26,3 +26,41 @@
  
  const firstName = 'Jonas';
  calcAge(1991);
+
+ //variables
+ console.log(me);
+//  console.log(job);
+//  console.log(year);
+
+ var me = 'Jonas';
+ let job = 'teacher';
+ const year = 1991;
+
+ //functions
+ console.log(addDecl(2, 3));
+ console.log(addExpr(2, 3));
+ console.log(addArrow(2,3));
+
+ function addDecl(a, b) { //can be used before being declared
+    return a + b;
+ };
+
+ const addExpr = function(a, b) {
+    return a + b;
+ };
+
+ const addArrow = (a, b) => a + b;
+
+//example:
+
+if(!numProducts) deleteShoppingCart();
+
+ var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+};
+
+var x = 1;
+let y = 2;
+const z = 3;
