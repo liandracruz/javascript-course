2374c3de79fb0ecb81f638dfbab66ae4cc51bebe
liandracruz <liandraluiza.02@gmail.com>
2025-10-27 10:50:05 -0300

    starting to implement the logic

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+/*
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent;
+document.querySelector('.score').textContent;
+document.querySelector('.guess').value;
+*/
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number!'
+    }
+})
