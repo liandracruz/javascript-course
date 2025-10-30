fe2471bc44f1c027d726fe2a9fa68d9caf70be7c
liandracruz <liandraluiza.02@gmail.com>
2025-10-30 10:11:11 -0300

    game logic ending

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -6,9 +6,14 @@ document.querySelector('.score').textContent;
 document.querySelector('.guess').value;
 */
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+/*
+my challenge attempt
+const inputStart = document.querySelector('.guess');
+let newGame = inputStart.value;
+*/
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -16,11 +21,16 @@ document.querySelector('.check').addEventListener('click', function() {
     if(!guess) {
         document.querySelector('.message').textContent = 'No number!';
     } else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.message').textContent = 'Correct Number!';document.querySelector('.number').textContent = secretNumber;
 
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
     } else if(guess > secretNumber) {
         if(score > 1) {
@@ -43,3 +53,28 @@ document.querySelector('.check').addEventListener('click', function() {
 
     }
 });
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
+
+/*
+my chellenge attempt
+document.querySelector('.again').addEventListener('click', function() {
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.number').textContent = '?';
+    newGame;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
+*/
